c1156cbfc5fb97bea3f01cb1489e85490cdcface
Ульяна <basalaeva.u.i>
2022-08-17 12:39:11 +0700

    third

--- src/main.java
+++ src/main.java
@@ -1,17 +1,8 @@
 public class main {
     public static void main(String[] args) {
-        int initialAccount = 200;
-        int addend = 150;
-        int bonus;
-
-        if (addend > 1000) {
-            bonus = addend / 100;
-        } else {
-            bonus = 0;
-        }
-        int finalAccount = initialAccount + addend + bonus;
-        System.out.println("Итоговый счет: " + finalAccount);
-
-        System.out.println("Количество бонусных рублей: " + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
